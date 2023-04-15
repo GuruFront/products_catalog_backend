36bb76ca2acd3363f488460c8359afe54333b092
Ruslan <ruslan.gunder@gmail.com>
2023-04-15 15:56:40 +0300

    files remove test;

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const productRouter = require('./routes/product_routes')
- const cors = require('cors');
-
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080
 
 const app = express();
-
 
 app.use(cors({
     origin: `${process.env.DEVELOPMENT ? 
@@ -14,10 +12,8 @@ app.use(cors({
         'https://product-catalog-front-git-main-ruslangunder-gmailcom.vercel.app'}`
 }));
 
-
 app.use(express.json())
 app.use('/api', productRouter)
-
 
 app.listen(PORT, () => {
     console.log('Server started on port: ', PORT)
