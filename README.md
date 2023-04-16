c6f38f9a5208eb691cc34744c95923593b1c8b14
Ruslan <ruslan.gunder@gmail.com>
2023-04-16 07:00:24 +0300

    readme edited;

---
+++ README.md
@@ -0,0 +1,16 @@
+## Install dependencies
+
+In the project directory:
+
+### `npm install`
+
+## Available Scripts
+
+### `npm run dev`
+
+Runs the server in the development mode.
+Server started successfully if you see {"server":"works"} there http://localhost:8080/api
+
+### `npm start`
+
+Runs the app in the production mode.
--- routes/product_routes.js
+++ routes/product_routes.js
@@ -5,5 +5,9 @@ const userController = require('../controllers/product.controller')
 router.post('/products', userController.getProductsByPage)
 router.get('/categories_list', userController.getProductCategories)
 
+//TODO remove it from product_routes
+router.get('/', (req, res)=> res.json({
+    server: 'works',
+}));
 
 module.exports = router
